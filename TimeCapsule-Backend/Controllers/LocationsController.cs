5c4448d66b7c3b0de4a7b10593c2c146775788be
malulleybovo <malulleybovo@wisc.edu>
2016-02-20 16:28:33 -0600

    locations calls made

---
+++ TimeCapsule-Backend/Controllers/LocationsController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Runtime.Serialization.Json;
+
+namespace WebRole1.Controllers
+{
+    public class LocationsController : Controller
+    {
+        // GET: Location
+        public DataContractJsonSerializer get(string name)
+        {
+            TimeCapsuleDBDataContext db = new TimeCapsuleDBDataContext();
+
+            IEnumerable<Location> locList = db.where();
+
+            DataContractJsonSerializer serializer;
+
+            return View();
+        }
+    }
+}
